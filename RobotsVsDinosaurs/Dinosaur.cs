5ed8163b1d34d778ebf88c91857ff459e8bcb46e
SammySupreme B <samb9382@gmail.com>
2020-05-11 12:04:54 -0500

    created member variables and constructors for classes dinosaur and robot

--- RobotsVsDinosaurs/Dinosaur.cs
+++ RobotsVsDinosaurs/Dinosaur.cs
@@ -7,6 +7,34 @@ using System.Threading.Tasks;
 namespace RobotsVsDinosaurs
 {
     class Dinosaur
-    {
+    {   //member variables
+        public string type;
+        public double health;
+        public double energy;
+        public double attackPower;
+
+        //constructor
+        public Dinosaur(string type, double health, double energy, double attackPower)
+        {
+            this.type = type;
+            this.health = health;
+            this.energy = energy;
+            this.attackPower = attackPower;
+        }
+
+
+
+        //member methods
+
+
+
+
+        //instantiation
+        Dinosaur dinosaurOne = new Dinosaur("");
+
+        Dinosaur dinosaurTwo = new Dinosaur();
+
+        Dinosaur dinosaurThree = new Dinosaur();
+
     }
 }
--- RobotsVsDinosaurs/Robot.cs
+++ RobotsVsDinosaurs/Robot.cs
@@ -8,5 +8,40 @@ namespace RobotsVsDinosaurs
 {
     class Robot
     {
+        // member variables
+        public string name;
+        public double health;
+        public double powerLevel;
+        public double attackPower;
+
+        //Constructor
+        public Robot(string name, double health, double powerLevel, double attackPower)
+        {
+            this.name = name;
+            this.health = health;
+            this.powerLevel = powerLevel;
+            this.attackPower = attackPower;
+
+        }
+
+
+
+
+
+        //member methods
+
+
+
+
+
+
+        //instantiation
+        Robot robotOne = new Robot("Optimus", 100, 50, 55);
+
+        Robot robotTwo = new Robot("Walle", 100, 15, 30);
+
+        Robot robotThree = new Robot("Rob", 100, 60, 80);
+
+
     }
 }
